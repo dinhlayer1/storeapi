b56a4aeed616f379164712cc91be311049880936
dinhlayer1 <razor16675@gmail.com>
2023-02-21 17:41:37 +0700

    host api live

---
+++ db/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+const connectDB = (uri) => {
+    return mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+module.exports = connectDB;
